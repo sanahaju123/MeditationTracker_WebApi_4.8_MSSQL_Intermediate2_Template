f9fbb5d321174f576741c94caa1fbeb6cbd1ddf0
sanahaju123 <88725494+sanahaju123@users.noreply.github.com>
2024-01-07 02:35:22 -0800

    Update Configuration.cs

--- MeditationTrackerApp/Migrations/Configuration.cs
+++ MeditationTrackerApp/Migrations/Configuration.cs
@@ -9,7 +9,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(MeditationTrackerApp.DAL.DatabaseContext context)
